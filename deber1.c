db3ee4ffce20d1872e5928eb0b53296d78c5ba5c
sy_sinai <sammy.porras@udla.edu.ec>
2023-01-13 15:32:17 -0500

    Inicio

--- deber1.c
+++ deber1.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 
+void suma(double a,double b);
+void resta(double a,double b);
+void multiplicacion(double a,double b);
+void division (double a,double b);
 
 int main(){
 
+    double n1,n2;
+
+    printf("Ingrese el primer numero:");
+    scanf("%lf",&n1);
+    printf("Ingrese el primer numero:");
+    scanf("%lf",&n2);
+    suma(n1,n2);
+    resta(n1,n2);
+    multiplicacion(n1,n2);
+    division(n1,n2);
     return 0;
 }
 
 void suma(double a,double b){
     double s=a+b;
-    
+    printf("La suma es: %.2lf \n",s);
+}
+
+void resta(double a,double b){
+    double s=a-b;
+    printf("La resta es: %.2lf \n",s);
+}
+
+void multiplicacion(double a,double b){
+    double s=a*b;
+    printf("La multiplicion es: %.2lf \n",s);
+}
+
+void division (double a,double b){
+    double s=a/b;
+    printf("La division es: %.2lf \n",s);
 }
